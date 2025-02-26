a9bb53b964b2f786bc31140a3a20f3912eebed55
LucaViola17 <130687039+LucaViola17@users.noreply.github.com>
2025-02-26 11:46:27 +0100

    Aggiunta struttura della classe Session

--- Session.java
+++ Session.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Session {
+package PACKAGE_NAME;
+
+public class Session {
+
+    protected Utente[] utenti;
+    protected Luogo[] luoghi;
+    protected Visita[] visite;
+
+    private Utente utenteAcceduto; //utenteCorrente?
+    private FileManager filemanager;
+
+    private void salva() {
+
+    }
+
+    private void carica() {
+
+    }
+
+    private void cambiaPassword(Utente utente) {
+
+    }
+
+
+    private void logic(Utente utente) {
+
+    }
+
 }
