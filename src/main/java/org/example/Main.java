a5e4a60d71655ddc81fcff5631e621b4fd2e72a7
LucaViola17 <130687039+LucaViola17@users.noreply.github.com>
2025-04-08 18:10:23 +0200

    Fix output/colori

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,14 @@
 package org.example;
 
+import utility.BelleStringhe;
 import utility.Controller;
+import utility.InputDati;
 
 public class Main {
     public static void main(String[] args) {
+        BelleStringhe.colori = true;
+        InputDati.newLineDifferente = false;
+        
         Controller controller = Controller.getIstance();
         controller.start();
     }
